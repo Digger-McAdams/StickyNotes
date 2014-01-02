c1e3892e5281e976e9a67fa5f663a317f01a9c06
Joe Flynn <rubikscube007@gmail.com>
2014-01-02 13:07:21 -0500

    Prev/Next Buttons are working

--- Stickynotes/urls.py
+++ Stickynotes/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns,url
 from Stickynotes import views
 
 urlpatterns=patterns('',
-	url(r'^$',views.index,name='index'),
+	url(r'^$',views.month,name='month'),
 	url(r'^month/$',views.month,name='month'),
+	url(r'^month/(\d+)/(\d+)/$', views.month,name='month'),
+	url(r'^month/(\d+)/(\d+)/(prev|next)/$',views.month,name='month'),
 )
--- Stickynotes/views.py
+++ Stickynotes/views.py
@@ -4,21 +4,41 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from datetime import date,datetime,timedelta
 import calendar
+
+month_name="Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec"
+month_name=month_name.split()
 def index(request):
 	return HttpResponse("Test")
-def month(request):
+def month(request,year,month,button=None):
 	context=RequestContext(request)
 	context_dict={}
-	month=datetime.now().month
-	year=datetime.now().year	
+	year=int(year)
+	month=int(month)
+	if button in ("prev", "next"):
+		now=date(year,month,15)
+		mdelta=timedelta(days=31)
+		if button=="next":
+			mod=mdelta
+		elif button=="prev":
+			mod=-mdelta
+		year,month=(now+mod).timetuple()[:2]
+	current_month=datetime.now().month
+	month_spell=month_name[month-1]
+	current_year=datetime.now().year	
+	current_day=datetime.now().day
 	cal=calendar.Calendar(6)
 	month_days=cal.itermonthdays(year,month)#how many days are in the month
 	context_dict['month']=month
 	context_dict['year']=year
+	context_dict['month_spell']=month_spell
 	lst=[[]]
 	week=0
 	for day in month_days:
+		current=False
 		lst[week].append((day))
+		if day:
+			if day==current_day and current_month==month and current_year==year:
+				current=True
 		if len(lst[week]) ==7:
 			lst.append([])
 			week+=1	
