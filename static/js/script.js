9127e5360e62a6ea48125e57e8d4198146485d5f
Joe Flynn <rubikscube007@gmail.com>
2014-01-08 22:51:08 -0500

    Added Add_entry link to calendar page

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ function hover(e){
 	console.log("got here");
 	console.log(e);
 	console.log(e.className);
-	if(e.className=="date"){
+	if(e.className=="date"|| e.className=="date note"){
 		e.style.backgroundColor='blue';
 		e.style.color='white';
 	}
@@ -15,6 +15,10 @@ function unhover(e){
 	if(e.className=="date"){
 		e.style.backgroundColor="#FFFFF0";	
 		e.style.color='black';
+	}
+	else if (e.className=="date note"){
+		e.style.backgroundColor="#00FFFF";
+		e.sytle.color='black';
 	}
 	else if(e.className=="current"){
 		e.style.backgroundColor="orange";	
